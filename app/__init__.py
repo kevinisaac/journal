f3f4c0bb2ab141950528edf7867a9fab88cb6ef3
Melvin Low <melvin.w.low@gmail.com>
2014-07-27 10:31:34 -0700

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,32 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from flask.ext.login import LoginManager
+from flask_oauth import OAuth
+
+
 app = Flask(__name__)
+
+# DB
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+# Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# OAuth
+oauth = OAuth()
+twitter = oauth.remote_app('twitter',
+    base_url='https://api.twitter.com/1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key='<your key here>',
+    consumer_secret='<your secret here>'
+)
+
+
+
 
 from app import views, models
