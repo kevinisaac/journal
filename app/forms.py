7150af2f0fdac67e0280236c65041111cd28f507
Melvin Low <melvin.w.low@gmail.com>
2014-07-28 03:05:30 -0700

    removed email field

--- app/forms.py
+++ app/forms.py
@@ -11,7 +11,6 @@ class LoginForm(Form):
 
 class RegistrationForm(Form):
    username = TextField('username', validators=[Required(), Length(max=16)])
-   email = TextField('email', [Required(), Email(), Length(max=128)])
    password = PasswordField('password', [
        Required(),
        Length(min=8, max=128),
